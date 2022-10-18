ee95aa97c609c84821b6b26e10cd7b5725decaaa
Hannah Jassat <99400438+hannjassat@users.noreply.github.com>
2022-10-18 16:14:37 +0200

    Update main.c

--- Prac4/main.c
+++ Prac4/main.c
@@ -121,7 +121,7 @@ uint32_t triangle_LUT[NS] = {16,32,48,64,80,96,112,128,
 //Calculate TIM2_Ticks
 
 //TIM2_TICKS = (TIM2CLK/F_SIGNAL)*NS
-uint32_t TIM2_Ticks = ((TIM2CLK/F_SIGNAL)*NS)/1000; // adjust the 100 based on what it looks like
+uint32_t TIM2_Ticks = (TIM2CLK/(F_SIGNAL*NS))/1000; // adjust the 100 based on what it looks like
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
